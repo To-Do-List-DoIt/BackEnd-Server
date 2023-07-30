dc15903eda4b9a2cf4cf1a702a7c7c5a32a385a4
Yusun Choi <nsuy.ch@gmail.com>
2023-07-30 23:19:18 +0900

    chore: Initialize Spring Boot application

---
+++ Do-It/src/test/java/com/choi/doit/DoItApplicationTests.java
@@ -0,0 +1,11 @@
+package com.choi.doit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoItApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
