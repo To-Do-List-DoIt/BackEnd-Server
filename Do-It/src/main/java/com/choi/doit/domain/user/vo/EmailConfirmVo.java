7ce990c6b08c11a96ae9293f83db25096e70bf47
Yusun Choi <nsuy.ch@gmail.com>
2023-08-09 02:45:07 +0900

    feat: Add EmailConfirmVo (#1)

---
+++ Do-It/src/main/java/com/choi/doit/domain/user/vo/EmailConfirmVo.java
@@ -0,0 +1,20 @@
+package com.choi.doit.domain.user.vo;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailConfirmVo {
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$")
+    String email;
+    @NotNull
+    String code;
+}
