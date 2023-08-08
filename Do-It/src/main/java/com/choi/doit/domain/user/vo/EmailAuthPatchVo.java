8360da4a6e3ae3ceb217412c05e90c0da6c37bbe
Yusun Choi <nsuy.ch@gmail.com>
2023-08-09 02:41:43 +0900

    feat: Add VOs for Email Join (#1)

---
+++ Do-It/src/main/java/com/choi/doit/domain/user/vo/EmailAuthPatchVo.java
@@ -0,0 +1,26 @@
+package com.choi.doit.domain.user.vo;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailAuthPatchVo {
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$")
+    @NotNull
+    private String prev_email;
+
+    @NotNull
+    private String code;
+
+    @Email
+    @NotNull
+    private String new_email;
+}
---
+++ Do-It/src/main/java/com/choi/doit/domain/user/vo/EmailJoinVo.java
@@ -0,0 +1,32 @@
+package com.choi.doit.domain.user.vo;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailJoinVo {
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$")
+    private String email;
+
+    @NotBlank
+    @NotNull
+    @Size(min = 5, max = 20)
+    private String password;
+
+    @NotNull
+    @Size(min = 3, max = 8)
+    private String nickname;
+
+    private MultipartFile profile;
+}
