2943f8dffb1a14d41cb50c3a58c4a7b0ac618031
Yusun Choi <nsuy.ch@gmail.com>
2023-08-14 17:57:29 +0900

    feat: Add MailUtil (#3)

---
+++ Do-It/src/main/java/com/choi/doit/global/util/MailUtil.java
@@ -0,0 +1,34 @@
+package com.choi.doit.global.util;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class MailUtil {
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(String to, String subject, String template) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(template, true);
+
+            javaMailSender.send(mimeMessage);
+
+            log.info("[Mail] Email sent successfully. -- " + to);
+        } catch (MessagingException e) {
+            log.error("[Mail] Email sending failed. -- " + to);
+            throw new RuntimeException(e);
+        }
+    }
+}
