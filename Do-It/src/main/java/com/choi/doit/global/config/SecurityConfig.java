48dd2173425f775e00a79b65ea734019beed193b
Yusun Choi <nsuy.ch@gmail.com>
2023-08-17 01:51:38 +0900

    chore: Add Security Configuration (#3)

--- Do-It/src/main/java/com/choi/doit/global/config/SecurityConfig.java
+++ Do-It/src/main/java/com/choi/doit/global/config/SecurityConfig.java
@@ -1,31 +1,93 @@
 package com.choi.doit.global.config;
 
+import com.choi.doit.domain.user.dao.UserRepository;
+import com.choi.doit.global.login.application.LoginDetailsService;
+import com.choi.doit.global.login.filter.JsonEmailPasswordAuthenticationFilter;
+import com.choi.doit.global.login.handler.LoginFailureHandler;
+import com.choi.doit.global.login.handler.LoginSuccessHandler;
+import com.choi.doit.global.util.ResponseUtil;
+import com.choi.doit.global.util.jwt.JwtUtil;
+import com.choi.doit.global.util.jwt.filter.JwtAuthenticationProcessingFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+@EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+    private final LoginDetailsService loginDetailsService;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+    private final ResponseUtil responseUtil;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/**").permitAll()
-                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .httpBasic(withDefaults());
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests.requestMatchers("/user/sign-up/**", "/user/login/**").permitAll()
+                                .anyRequest().authenticated());
+
+        http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
+        http.addFilterBefore(jwtAuthenticationProcessingFilter(), JsonEmailPasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(loginDetailsService);
+
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler(userRepository, objectMapper, responseUtil, jwtUtil);
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler(responseUtil);
+    }
+
+    @Bean
+    public JsonEmailPasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
+        JsonEmailPasswordAuthenticationFilter customJsonUsernamePasswordLoginFilter
+                = new JsonEmailPasswordAuthenticationFilter(objectMapper);
+        customJsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+        customJsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
+        customJsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return customJsonUsernamePasswordLoginFilter;
+    }
+
+    @Bean
+    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
+        return new JwtAuthenticationProcessingFilter(jwtUtil, responseUtil);
     }
 }
