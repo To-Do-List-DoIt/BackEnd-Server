a313031b8b057676b75093782a3dbecef930c2da
Yusun Choi <nsuy.ch@gmail.com>
2023-08-17 22:49:07 +0900

    feat: Add OAuthUserInfoDto (#5)

---
+++ Do-It/src/main/java/com/choi/doit/domain/user/vo/OAuthUserInfoDto.java
@@ -0,0 +1,17 @@
+package com.choi.doit.domain.user.vo;
+
+import com.choi.doit.domain.user.domain.Provider;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OAuthUserInfoDto {
+    private Provider provider;
+    private String email;
+    private String nickname;
+    private String password;
+    private String profile_image_path;
+}
