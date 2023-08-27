2d3e374167146c3dfcf0e963147bc4bd158bd46b
Yusun Choi <nsuy.ch@gmail.com>
2023-08-28 02:20:48 +0900

    feat: Add TodoApi (#7)

---
+++ Do-It/src/main/java/com/choi/doit/domain/todo/api/TodoApi.java
@@ -0,0 +1,73 @@
+package com.choi.doit.domain.todo.api;
+
+import com.choi.doit.domain.todo.application.TodoCUDService;
+import com.choi.doit.domain.todo.application.TodoRService;
+import com.choi.doit.domain.todo.dto.request.EditTodoRequestDto;
+import com.choi.doit.domain.todo.dto.request.NewTodoRequestDto;
+import com.choi.doit.domain.todo.dto.response.*;
+import com.choi.doit.global.common.response.DataResponseDto;
+import com.choi.doit.global.common.response.ResponseDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/todo")
+public class TodoApi {
+    private final TodoCUDService todoCUDService;
+    private final TodoRService todoRService;
+
+    @PostMapping
+    public ResponseEntity<ResponseDto> addNew(@RequestHeader(value = "Authorization") String authorization, @Valid NewTodoRequestDto newTodoRequestDto) {
+        NewTodoResponseDto newTodoResponseDto = todoCUDService.addNewTodo(newTodoRequestDto);
+
+        return ResponseEntity.status(201).body(DataResponseDto.of(newTodoResponseDto, 201));
+    }
+
+    @DeleteMapping("/{todo-id}")
+    public ResponseEntity<ResponseDto> delete(@RequestHeader(value = "Authorization") String authorization, @PathVariable(value = "todo-id") Long todo_id) {
+        todoCUDService.deleteTodo(todo_id);
+
+        return ResponseEntity.status(201).body(ResponseDto.of(201));
+    }
+
+    @PatchMapping("/{todo-id}")
+    public ResponseEntity<ResponseDto> edit(@RequestHeader(value = "Authorization") String authorization, @PathVariable(value = "todo-id") Long todo_id, @Valid EditTodoRequestDto editTodoRequestDto) {
+        todoCUDService.editTodo(todo_id, editTodoRequestDto);
+
+        return ResponseEntity.status(201).body(ResponseDto.of(201));
+    }
+
+    @PatchMapping("/check/{todo-id}")
+    public ResponseEntity<ResponseDto> setCheck(@RequestHeader(value = "Authorization") String authorization, @PathVariable(value = "todo-id") Long todo_id) {
+        CheckResponseDto checkResponseDto = todoCUDService.setCheck(todo_id);
+
+        return ResponseEntity.status(201).body(DataResponseDto.of(checkResponseDto, 201));
+    }
+
+    @GetMapping("/day-all")
+    public ResponseEntity<ResponseDto> getDay(@RequestHeader(value = "Authorization") String authorization, @RequestParam @NotNull String date) {
+        DayTodoDto dayTodoDto = todoRService.readDay(date);
+
+        return ResponseEntity.status(201).body(DataResponseDto.of(dayTodoDto, 201));
+    }
+
+    @GetMapping("/day")
+    public ResponseEntity<ResponseDto> getDayCategory(@RequestHeader(value = "Authorization") String authorization, @RequestParam @NotNull String date, @RequestParam @NotNull String category) {
+        CategoryDayTodoDto categoryDayTodoDto = todoRService.readCategoryDay(category, date);
+
+        return ResponseEntity.status(201).body(DataResponseDto.of(categoryDayTodoDto, 201));
+    }
+
+    @GetMapping("/month")
+    public ResponseEntity<ResponseDto> getMonthCount(@RequestHeader(value = "Authorization") String authorization, @RequestParam @NotNull String date) {
+        MonthCountDto monthCountDto = todoRService.readMonthCount(date);
+
+        return ResponseEntity.status(201).body(DataResponseDto.of(monthCountDto, 201));
+    }
+}
