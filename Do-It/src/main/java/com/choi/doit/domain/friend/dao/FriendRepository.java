5ba6be369901313b64e3f07aa70fdfae11adb9ef
Yusun Choi <nsuy.ch@gmail.com>
2023-09-16 02:14:17 +0900

    feat: Add Friend entity (#13)

---
+++ Do-It/src/main/java/com/choi/doit/domain/friend/dao/FriendRepository.java
@@ -0,0 +1,17 @@
+package com.choi.doit.domain.friend.dao;
+
+import com.choi.doit.domain.friend.domain.FriendEntity;
+import com.choi.doit.domain.friend.dto.FriendItemDto;
+import com.choi.doit.domain.user.domain.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
+
+public interface FriendRepository extends JpaRepository<FriendEntity, Long> {
+
+    @Query("select new com.choi.doit.domain.friend.dto.FriendItemDto(f.friendUser) " +
+            "from Friend f " +
+            "where f.user = :user")
+    ArrayList<FriendItemDto> findAllByUser(UserEntity user);
+}
