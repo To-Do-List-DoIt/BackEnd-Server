67b98f07d39cfcaa3a4aee6099aa13ec4579d3bd
Yusun Choi <nsuy.ch@gmail.com>
2023-09-16 02:36:00 +0900

    feat: Add default categories (#14)

---
+++ Do-It/src/main/java/com/choi/doit/domain/todo/application/TodoDefaultSettingService.java
@@ -0,0 +1,31 @@
+package com.choi.doit.domain.todo.application;
+
+import com.choi.doit.domain.model.CategoryEntity;
+import com.choi.doit.domain.model.UserEntity;
+import com.choi.doit.domain.todo.dao.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class TodoDefaultSettingService {
+    private final CategoryRepository categoryRepository;
+    
+    // 기본 카테고리 추가
+    @Transactional
+    public void addDefaultCategory(UserEntity user) {
+        final String DEFAULT_1_NAME = "공부";
+        final String DEFAULT_2_NAME = "일상";
+        final String DEFAULT_3_NAME = "운동";
+        final String DEFAULT_1_COLOR = "FF5E38";
+        final String DEFAULT_2_COLOR = "0052FE";
+        final String DEFAULT_3_COLOR = "FFD917";
+
+        categoryRepository.save(new CategoryEntity(user, DEFAULT_1_NAME, DEFAULT_1_COLOR));
+        categoryRepository.save(new CategoryEntity(user, DEFAULT_2_NAME, DEFAULT_2_COLOR));
+        categoryRepository.save(new CategoryEntity(user, DEFAULT_3_NAME, DEFAULT_3_COLOR));
+    }
+}
