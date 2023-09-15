6622a1f56cfeb417263bd01cc3418444b267f5f2
Yusun Choi <nsuy.ch@gmail.com>
2023-09-16 02:14:27 +0900

    feat: Add FriendRequest entity (#13)

---
+++ Do-It/src/main/java/com/choi/doit/domain/friend/dao/FriendRequestRepository.java
@@ -0,0 +1,24 @@
+package com.choi.doit.domain.friend.dao;
+
+import com.choi.doit.domain.friend.domain.FriendRequestEntity;
+import com.choi.doit.domain.friend.dto.FriendItemDto;
+import com.choi.doit.domain.user.domain.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public interface FriendRequestRepository extends JpaRepository<FriendRequestEntity, Long> {
+    Optional<FriendRequestEntity> findByUserAndTargetUser(UserEntity user, UserEntity targetUser);
+
+    @Query("select new com.choi.doit.domain.friend.dto.FriendItemDto(f.targetUser) " +
+            "from FriendRequest f " +
+            "where f.user = :user")
+    ArrayList<FriendItemDto> findAllByUser(UserEntity user);
+
+    @Query("select new com.choi.doit.domain.friend.dto.FriendItemDto(f.user) " +
+            "from FriendRequest f " +
+            "where f.targetUser = :targetUser")
+    ArrayList<FriendItemDto> findAllByTargetUser(UserEntity targetUser);
+}
